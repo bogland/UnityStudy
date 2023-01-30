d1525546608f5742501dd5cbb5c266780898fa69
oh <guruwang2@gmail.com>
2023-01-30 23:06:06 +0900

    무한맵 구현중

---
+++ vampire/vampire/Assets/Scripts/Player.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Player : MonoBehaviour
+{
+    public Vector2 inputVec;
+    public float speed = 3f;
+    Rigidbody2D rigidbody;
+    SpriteRenderer sprite;
+    Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+        sprite = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    private void FixedUpdate()
+    {
+        Vector2 nextVec = inputVec.normalized * speed*Time.fixedDeltaTime;
+        rigidbody.MovePosition(rigidbody.position + nextVec);
+    }
+
+    private void LateUpdate()
+    {
+        animator.SetFloat("speed", inputVec.magnitude);
+        var isLeft = inputVec.x < 0;
+        sprite.flipX = isLeft;
+    }
+
+    void OnMove(InputValue value)
+    {
+        inputVec = value.Get<Vector2>();
+    }
+}
